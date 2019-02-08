721a5bfe8c2bdaeffb158ce4e42721e095e1b8ca
Angelo Corsaro <angelo@icorsaro.net>
2019-02-08 12:57:59 +0100

    added mutable abuf

---
+++ lib/abuf.ml
@@ -0,0 +1,221 @@
+open Identifiers
+
+module Id = NumId.Make(Int64)
+
+type byte = Abytes.byte
+type bigstring = Abytes.bigstring
+type t = { 
+  id : Id.t;
+  buffer : Abytes.t;
+  mutable r_pos : int;
+  mutable w_pos : int;
+  mutable r_mark : int;
+  mutable w_mark : int;
+}
+
+let compare a b = Id.compare a.id b.id 
+
+let equal a b = Id.equal a.id b.id
+
+let from_abytes bs =
+  { 
+    id = Id.next_id ();
+    buffer = bs;
+    r_pos = 0;
+    w_pos = Abytes.capacity bs;
+    r_mark = 0;
+    w_mark = 0;
+  }
+
+let from_bytes ?(grow=0) bs = from_abytes (Abytes.from_bytes ~grow  bs)
+
+let from_bigstring ?(grow=0) bs = from_abytes (Abytes.from_bigstring ~grow  bs)
+
+let create_bigstring ?(grow=0) len =
+  { 
+    id = Id.next_id ();
+    buffer = Abytes.create_bigstring ~grow len;
+    r_pos = 0;
+    w_pos = 0;
+    r_mark = 0;
+    w_mark = 0;
+  }
+
+let create_bytes ?(grow=0) len =
+  { 
+    id = Id.next_id ();
+    buffer = Abytes.create_bytes ~grow len;
+    r_pos = 0;
+    w_pos = 0;
+    r_mark = 0;
+    w_mark = 0;
+  }
+
+let wrap ?(grow=0) bslist = 
+  let abytes = List.map (fun buf -> buf.buffer) bslist in
+  { 
+    id = Id.next_id ();
+    buffer = Abytes.wrap ~grow abytes;
+    r_pos = 0;
+    w_pos = 0;
+    r_mark = 0;
+    w_mark = 0;
+  }
+
+let slice from len buf = 
+    { 
+      id = Id.next_id ();
+      buffer =  Abytes.slice from len buf.buffer;
+      r_pos = 0;
+      w_pos = 0;
+      r_mark = 0;
+      w_mark = 0;
+    } 
+  
+let capacity buf = Abytes.capacity buf.buffer
+
+let clear buf = buf.r_pos <- 0; buf.w_pos <- 0
+
+let r_pos buf = buf.r_pos
+
+let set_r_pos i buf = 
+  if i >= 0 && i <= buf.w_pos
+  then buf.r_pos <- i
+  else raise @@ Apero.Exception (`OutOfBounds (`Msg (
+    Printf.sprintf "A_buf.set_r_pos with %d out of (0 .. %d)" i buf.w_pos)))
+
+let mark_r_pos buf = buf.r_mark <- buf.r_pos
+
+let reset_r_pos buf = buf.r_pos <- buf.r_mark
+
+let w_pos buf = buf.w_pos
+
+let set_w_pos i buf = 
+  if i >= buf.r_pos && i <= capacity buf
+  then buf.w_pos <-  i
+  else raise @@ Apero.Exception (`OutOfBounds (`Msg (
+    Printf.sprintf "A_buf.set_w_pos with %d out of (%d .. %d)" i buf.r_pos (capacity buf))))
+
+let mark_w_pos buf = buf.w_mark <- buf.w_pos
+
+let reset_w_pos buf = buf.w_pos <- buf.w_mark
+
+
+let readable_bytes buf = (w_pos buf) - (r_pos buf)
+
+let readable buf = readable_bytes buf > 0
+
+let writable_bytes buf = (capacity buf) - (w_pos buf)
+
+let writable buf = writable_bytes buf > 0
+
+
+let skip n buf = set_r_pos (buf.r_pos + n) buf 
+
+let blit ~src ~src_idx ~dst ~dst_idx ~len = 
+  Abytes.blit ~src:src.buffer ~src_idx ~dst:dst.buffer ~dst_idx ~len
+
+let blit_from_bytes ~src ~src_idx ~dst ~dst_idx ~len = 
+  Abytes.blit_from_bytes ~src ~src_idx ~dst:dst.buffer ~dst_idx ~len
+  
+let blit_to_bytes ~src ~src_idx ~dst ~dst_idx ~len = 
+  Abytes.blit_to_bytes ~src:src.buffer ~src_idx ~dst ~dst_idx ~len
+
+let blit_from_bigstring ~src ~src_idx ~dst ~dst_idx ~len = 
+  Abytes.blit_from_bigstring ~src ~src_idx ~dst:dst.buffer ~dst_idx ~len
+  
+let blit_to_bigstring ~src ~src_idx ~dst ~dst_idx ~len = 
+  Abytes.blit_to_bigstring ~src:src.buffer ~src_idx ~dst ~dst_idx ~len
+
+let blit_from_abytes ~src ~src_idx ~dst ~dst_idx ~len = 
+  Abytes.blit ~src ~src_idx ~dst:dst.buffer ~dst_idx ~len
+  
+let blit_to_abytes ~src ~src_idx ~dst ~dst_idx ~len = 
+  Abytes.blit ~src:src.buffer ~src_idx ~dst ~dst_idx ~len
+
+
+let read_byte buf = 
+  let b = Abytes.get_byte ~at:buf.r_pos buf.buffer in 
+  buf.r_pos <- buf.r_pos+1 ; b
+  
+
+let read_bytes len buf = 
+  let bs = Abytes.get_bytes ~at:buf.r_pos len buf.buffer in 
+  buf.r_pos <- buf.r_pos + len ; bs
+ 
+let read_abytes len buf = 
+  let bs = Abytes.get_abytes ~at:buf.r_pos len buf.buffer in 
+  buf.r_pos <- buf.r_pos+len; bs
+  
+
+let read_bigstring len buf = 
+  let bs = Abytes.get_bigstring ~at:buf.r_pos len buf.buffer in 
+  buf.r_pos <- buf.r_pos+len ; bs
+  
+
+let read_buf len buf = 
+  let bs = from_bigstring @@ Abytes.get_bigstring ~at:buf.r_pos len buf.buffer in 
+  buf.r_pos <- buf.r_pos+len; bs
+   
+
+let get_byte ~at buf =
+  if at + 1 <= buf.w_pos 
+  then Abytes.get_byte ~at buf.buffer
+  else raise @@ Apero.Exception (`OutOfBounds (`Msg "A_buf.get_byte"))
+
+let get_bytes ~at len buf = 
+  if at + len <= buf.w_pos 
+  then Abytes.get_bytes ~at len buf.buffer
+  else raise @@ Apero.Exception (`OutOfBounds (`Msg "A_buf.get_bytes"))
+
+let get_abytes ~at len buf = 
+  if at + len <= buf.w_pos 
+  then Abytes.get_abytes ~at len buf.buffer
+  else raise @@ Apero.Exception (`OutOfBounds (`Msg "A_buf.get_abytes"))
+
+let get_bigstring ~at len buf = 
+  if at + len <= buf.w_pos
+  then Abytes.get_bigstring ~at len buf.buffer
+  else raise @@ Apero.Exception (`OutOfBounds (`Msg "A_buf.get_bigstring"))
+
+let get_buf ~at len buf = 
+  if at + len <= buf.w_pos
+  then from_bigstring @@ Abytes.get_bigstring ~at len buf.buffer     
+  else raise @@ Apero.Exception (`OutOfBounds (`Msg "A_buf.get_buf"))
+
+
+let write_byte b buf = 
+  Abytes.set_byte b ~at:buf.w_pos buf.buffer ;
+  buf.w_pos <- buf.w_pos+1
+  
+
+let write_bytes bs buf = 
+  Abytes.set_bytes ~at:buf.w_pos bs buf.buffer ;
+  buf.w_pos <- buf.w_pos+(Bytes.length bs)
+
+let write_abytes bs buf = 
+  Abytes.set_abytes ~at:buf.w_pos bs buf.buffer ;
+  buf.w_pos <- buf.w_pos+(Abytes.capacity bs)
+  
+let write_bigstring bs buf = 
+  Abytes.set_bigstring ~at:buf.w_pos bs buf.buffer ;
+  buf.w_pos <- buf.w_pos+(Bigstringaf.length bs)
+  
+let write_buf bs buf = 
+  Abytes.set_abytes ~at:buf.w_pos bs.buffer buf.buffer ;
+  buf.w_pos <- buf.w_pos+(capacity bs)
+
+let set_byte b ~at buf = Abytes.set_byte b ~at buf.buffer 
+  
+let set_bytes bs ~at buf = Abytes.set_bytes bs ~at buf.buffer 
+
+let set_abytes bs ~at buf = Abytes.set_abytes bs ~at buf.buffer 
+
+let set_bigstring bs ~at buf = Abytes.set_bigstring bs ~at buf.buffer 
+
+let set_buf bs ~at buf = Abytes.set_abytes bs.buffer ~at buf.buffer 
+
+let hexdump ?separator:(sep="") buf = Abytes.hexdump ~separator:sep buf.buffer
+    
+let to_string buf =
+  "(r_pos: " ^ (string_of_int buf.r_pos) ^ ", w_pos: " ^ (string_of_int buf.w_pos) ^ " content: " ^ (hexdump buf ~separator:":")
---
+++ lib/abytes.ml
@@ -0,0 +1,301 @@
+open Identifiers
+
+module Id = NumId.Make(Int64)
+
+type byte = char
+type bigstring = (char, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
+
+type buffer = | Bytes of bytes | Bigstr of bigstring | Bufset of t list
+and  t = { 
+  id : Id.t;
+  buffer : buffer;
+  mutable offset : int; 
+  mutable capacity : int; 
+  grow : int;
+}
+
+let compare a b = Id.compare a.id b.id 
+
+let equal a b = Id.equal a.id b.id
+
+
+let from_bigstring ?(grow=0) bs =
+  { 
+    id = Id.next_id ();
+    buffer = Bigstr bs;
+    offset = 0;
+    capacity = Bigstringaf.length bs;
+    grow;
+  }
+
+let from_bytes ?(grow=0) bs =
+  { 
+    id = Id.next_id ();
+    buffer = Bytes bs;
+    offset = 0;
+    capacity = Bytes.length bs;
+    grow;
+  }
+
+let create_bigstring ?(grow=0) len = from_bigstring ~grow (Bigstringaf.create len)
+
+let create_bytes ?(grow=0) len = from_bytes ~grow (Bytes.create len)
+
+let capacity bs = bs.capacity
+
+let wrap ?(grow=0) bslist = 
+  { 
+    id = Id.next_id ();
+    buffer = Bufset bslist;
+    offset = 0;
+    capacity = List.fold_left (fun accu bs -> accu + capacity bs) 0 bslist;
+    grow;
+  }
+
+let slice from len bs = 
+  if from > 0 && len > 0 && (from + len) <= bs.capacity  then
+    { 
+      id = Id.next_id ();
+      buffer = bs.buffer;
+      offset = bs.offset + from;
+      capacity = len;
+      grow = 0;
+    }
+  else raise @@ Apero.Exception (`OutOfBounds (`Msg (
+    Printf.sprintf "Abytes.slice")))
+
+let expand n bs = 
+  match bs.buffer with 
+  | Bytes _ -> wrap ~grow:n [bs; create_bytes n]
+  | Bigstr _ -> wrap ~grow:n [bs; create_bigstring n]
+  | Bufset b -> wrap ~grow:n (List.append b [create_bigstring n]) (* TODO *)
+
+let rec blit_from_bytes ~src ~src_idx ~dst ~dst_idx ~len = 
+  if src_idx >= 0 && len >= 0 && src_idx + len <= Bytes.length src && dst_idx >= 0 
+  then 
+    if dst_idx + len <= capacity dst 
+    then
+      match dst.buffer with 
+      | Bytes b -> (Bytes.blit src src_idx b (dst.offset + dst_idx) len)
+      | Bigstr b -> (Bigstringaf.blit_from_bytes src ~src_off:src_idx b ~dst_off:(dst.offset + dst_idx) ~len)
+      | Bufset b -> 
+        let rec blit_from_bytes_to_set ~src ~src_idx ~dst ~dst_idx ~len = 
+          match dst with 
+          | [] -> ()
+          | hd :: tl -> 
+            if capacity hd > dst_idx 
+            then 
+              let hd_writable = capacity hd - dst_idx in 
+              if hd_writable >= len
+              then blit_from_bytes ~src ~src_idx ~dst:hd ~dst_idx ~len
+              else 
+                begin 
+                  blit_from_bytes ~src ~src_idx ~dst:hd ~dst_idx ~len:hd_writable ;                   
+                  blit_from_bytes_to_set ~src ~src_idx:(src_idx - hd_writable) ~dst:tl ~dst_idx:0 ~len:(len - hd_writable)                  
+                end
+            else 
+              blit_from_bytes_to_set ~src ~src_idx ~dst:tl ~dst_idx:(dst_idx - capacity hd) ~len
+        in 
+        blit_from_bytes_to_set ~src ~src_idx ~dst:b ~dst_idx ~len
+    else
+      match dst.grow with 
+      | 0 -> raise @@ Apero.Exception (`OutOfBounds (`Msg "Abytes.blit_from_bytes"))
+      | n -> blit_from_bytes ~src ~src_idx ~dst:(expand n dst) ~dst_idx ~len
+  else raise @@ Apero.Exception (`OutOfBounds (`Msg "Abytes.blit_from_bytes"))
+  
+let rec blit_to_bytes ~src ~src_idx ~dst ~dst_idx ~len = 
+  if src_idx >= 0 && len >= 0 && src_idx + len <= capacity src 
+  then
+    match src.buffer with 
+    | Bytes b -> (Bytes.blit b (src.offset + src_idx) dst dst_idx len)
+    | Bigstr b -> (Bigstringaf.blit_to_bytes b ~src_off:(src.offset + src_idx) dst ~dst_off:dst_idx ~len)
+    | Bufset b -> 
+      let rec blit_set_to_bytes ~src ~src_idx ~dst ~dst_idx ~len = 
+        match src with 
+        | [] ->  ()
+        | hd :: tl -> 
+          if capacity hd > src_idx 
+          then 
+            let hd_readable = capacity hd - src_idx in
+            if hd_readable >= len
+            then blit_to_bytes ~src:hd ~src_idx ~dst ~dst_idx ~len
+            else 
+              begin 
+                blit_to_bytes ~src:hd ~src_idx ~dst ~dst_idx ~len:hd_readable ;
+                blit_set_to_bytes ~src:tl ~src_idx:0 ~dst ~dst_idx:(dst_idx + hd_readable) ~len:(len - hd_readable)
+              end               
+          else 
+            blit_set_to_bytes ~src:tl ~src_idx:(src_idx - capacity hd) ~dst ~dst_idx ~len
+      in 
+      blit_set_to_bytes ~src:b ~src_idx ~dst ~dst_idx ~len
+  else 
+    raise @@ Apero.Exception (`OutOfBounds (`Msg "Abytes.blit_to_bytes")) 
+
+let rec blit_from_bigstring ~src ~src_idx ~dst ~dst_idx ~len = 
+  if src_idx >= 0 && len >= 0 && src_idx + len <= Bigstringaf.length src && dst_idx >= 0 
+  then 
+    if dst_idx + len <= capacity dst 
+    then
+      match dst.buffer with 
+      | Bytes b -> (Bigstringaf.blit_to_bytes src ~src_off:src_idx b ~dst_off:(dst.offset + dst_idx) ~len)
+      | Bigstr b -> (Bigstringaf.blit src ~src_off:src_idx b ~dst_off:(dst.offset + dst_idx) ~len)
+      | Bufset b -> 
+        let rec blit_from_bigstring_to_set ~src ~src_idx ~dst ~dst_idx ~len = 
+          match dst with 
+          | [] -> ()
+          | hd :: tl -> 
+            if capacity hd > dst_idx 
+            then 
+              let hd_writable = capacity hd - dst_idx in 
+              if hd_writable >= len
+              then blit_from_bigstring ~src ~src_idx ~dst:hd ~dst_idx ~len
+              else 
+                begin 
+                  blit_from_bigstring ~src ~src_idx ~dst:hd ~dst_idx ~len:hd_writable ;
+                  blit_from_bigstring_to_set ~src ~src_idx:(src_idx - hd_writable) ~dst:tl ~dst_idx:0 ~len:(len - hd_writable)
+                end
+            else 
+              blit_from_bigstring_to_set ~src ~src_idx ~dst:tl ~dst_idx:(dst_idx - capacity hd) ~len
+        in 
+        blit_from_bigstring_to_set ~src ~src_idx ~dst:b ~dst_idx ~len
+    else
+      match dst.grow with 
+      | 0 -> raise @@ Apero.Exception (`OutOfBounds (`Msg "Abytes.blit_from_bigstring"))
+      | n -> blit_from_bigstring ~src ~src_idx ~dst:(expand n dst) ~dst_idx ~len
+  else 
+    raise @@ Apero.Exception (`OutOfBounds (`Msg "Abytes.blit_from_bigstring"))
+  
+let rec blit_to_bigstring ~src ~src_idx ~dst ~dst_idx ~len = 
+  if src_idx >= 0 && len >= 0 && src_idx + len <= capacity src 
+  then
+    match src.buffer with 
+    | Bytes b -> (Bigstringaf.blit_from_bytes b ~src_off:(src.offset + src_idx) dst ~dst_off:dst_idx ~len)
+    | Bigstr b -> (Bigstringaf.blit b ~src_off:(src.offset + src_idx) dst ~dst_off:dst_idx ~len)
+    | Bufset b -> 
+      let rec blit_set_to_bigstring ~src ~src_idx ~dst ~dst_idx ~len = 
+        match src with 
+        | [] -> ()
+        | hd :: tl -> 
+          if capacity hd > src_idx 
+          then 
+            let hd_readable = capacity hd - src_idx in
+            if hd_readable >= len
+            then blit_to_bigstring ~src:hd ~src_idx ~dst ~dst_idx ~len:len
+            else
+              begin 
+                blit_to_bigstring ~src:hd ~src_idx ~dst ~dst_idx ~len:hd_readable ;
+                blit_set_to_bigstring ~src:tl ~src_idx:0 ~dst ~dst_idx:(dst_idx + hd_readable) ~len:(len - hd_readable)
+              end
+          else 
+            blit_set_to_bigstring ~src:tl ~src_idx:(src_idx - capacity hd) ~dst ~dst_idx ~len
+      in 
+      blit_set_to_bigstring ~src:b ~src_idx ~dst ~dst_idx ~len
+  else 
+    raise @@ Apero.Exception (`OutOfBounds (`Msg "Abytes.blit_to_bigstring")) 
+
+let rec blit ~src ~src_idx ~dst ~dst_idx ~len = 
+  if src_idx >= 0 && len >= 0 && src_idx + len <= capacity src && dst_idx >= 0 
+  then 
+    if dst_idx + len <= capacity dst 
+    then
+      match src.buffer with 
+      | Bytes b -> blit_from_bytes ~src:b ~src_idx:(src.offset + src_idx) ~dst ~dst_idx ~len
+      | Bigstr b -> blit_from_bigstring ~src:b ~src_idx:(src.offset + src_idx) ~dst ~dst_idx ~len
+      | Bufset b -> 
+        let rec blit_fromto_set ~src ~src_idx ~dst ~dst_idx ~len = 
+          match src with 
+          | [] -> ()
+          | hd :: tl -> 
+            if capacity hd > src_idx 
+            then 
+              let hd_readable = capacity hd - src_idx in
+              if hd_readable >= len
+              then blit ~src:hd ~src_idx ~dst ~dst_idx ~len:len
+              else 
+                begin 
+                  blit ~src:hd ~src_idx ~dst ~dst_idx ~len:hd_readable ;                
+                  blit_fromto_set ~src:tl ~src_idx:0 ~dst ~dst_idx:(dst_idx + hd_readable) ~len:(len - hd_readable)
+                end
+            else 
+              blit_fromto_set ~src:tl ~src_idx:(src_idx - capacity hd) ~dst ~dst_idx ~len
+        in
+        blit_fromto_set ~src:b ~src_idx ~dst ~dst_idx ~len
+    else
+      match dst.grow with 
+      | 0 -> raise @@ Apero.Exception (`OutOfBounds (`Msg "Abytes.blit"))
+      | n -> blit ~src ~src_idx ~dst:(expand n dst) ~dst_idx ~len
+  else raise @@ Apero.Exception (`OutOfBounds (`Msg "Abytes.blit"))
+
+let rec get_byte ~at bs =
+  if at >= 0 && at + 1 <= capacity bs then
+    begin
+      (match bs.buffer with 
+      | Bytes b -> (Bytes.get b (bs.offset + at))
+      | Bigstr b -> (Bigstringaf.get b (bs.offset + at))
+      | Bufset b -> 
+        let rec get_byte_from_set at set = 
+          match set with 
+          | [] -> raise @@ Apero.Exception (`OutOfBounds (`Msg "Abytes.get_byte"))
+          | hd :: tl -> 
+            if capacity hd > at 
+            then get_byte ~at hd
+            else get_byte_from_set (at - capacity hd) tl in 
+        get_byte_from_set at b)
+    end
+  else 
+    raise @@ Apero.Exception (`OutOfBounds (`Msg "Abytes.get_byte"))
+
+let get_bytes ~at len bs = 
+  let dst = Bytes.create len in
+  blit_to_bytes ~src:bs ~src_idx:at ~dst ~dst_idx:0 ~len ;
+  dst 
+
+let get_bigstring ~at len bs = 
+  let dst = Bigstringaf.create len in
+  blit_to_bigstring ~src:bs ~src_idx:at ~dst ~dst_idx:0 ~len ; dst 
+
+let get_abytes ~at len bs = from_bigstring @@ get_bigstring ~at len bs
+
+let rec set_byte c ~at bs = 
+  if at >= 0 then 
+    begin
+      if at + 1 <= capacity bs then
+        begin
+          match bs.buffer with 
+          | Bytes b -> Bytes.set b (bs.offset + at) c
+          | Bigstr b -> Bigstringaf.set b (bs.offset + at) c
+          | Bufset b -> 
+            let rec set_byte_to_set at set = 
+              match set with 
+              | [] -> raise @@ Apero.Exception (`OutOfBounds (`Msg "Abytes.set_byte"))
+              | hd :: tl -> 
+                if capacity hd > at 
+                then set_byte c ~at hd
+                else set_byte_to_set (at - capacity hd) tl in 
+            set_byte_to_set at b
+        end
+      else
+        match bs.grow with 
+        | 0 -> raise @@ Apero.Exception (`OutOfBounds (`Msg "Abytes.set_byte"))
+        | n -> set_byte ~at c (expand n bs) 
+    end
+  else raise @@ Apero.Exception (`OutOfBounds (`Msg "Abytes.set_byte"))
+
+let set_bytes src ~at bs = 
+  blit_from_bytes ~src ~src_idx:0 ~dst:bs ~dst_idx:at ~len:(Bytes.length src)
+
+let set_bigstring src ~at bs = 
+  blit_from_bigstring ~src ~src_idx:0 ~dst:bs ~dst_idx:at ~len:(Bigstringaf.length src)
+
+let set_abytes src ~at bs = 
+  blit ~src ~src_idx:0 ~dst:bs ~dst_idx:at ~len:(capacity src) 
+
+let hexdump ?separator:(sep="") bs =
+  let rec hexdump bs idx =
+    if idx < bs.capacity then 
+    (Printf.sprintf "%02x%s" (get_byte ~at:idx bs |> int_of_char ) sep ) ^ (hexdump bs (idx+1))
+    else "" in 
+  hexdump bs 0
+    
+let to_string bs =
+  "(capacity: " ^ (string_of_int bs.capacity) ^ " content: " ^ (hexdump bs ~separator:":") 
